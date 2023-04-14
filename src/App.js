5652a13ab99c88f3af89bd05f5e4574522ab4ac8
ashok kumawat <100012346+Anawadiya7610@users.noreply.github.com>
2023-04-14 11:49:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [src, setSrc] = useState(); // [], {name: 's', s:'sds'}, 
+  const [list, setList] = useState(null);
+
+  
+
+  const getImage = async () => {
+    const response = await fetch("https://dog.ceo/api/breeds/image/random");
+    const data = await response.json();
+    setSrc(data.message);
+    console.log(data);
+  };
+   
+  const getImageByBreed = async (breed) => {
+    const response = await fetch(`https://dog.ceo/api/breed/${breed}/images/random`);
+    const data = await response.json();
+    setSrc(data.message);
+    console.log(data);
+  };
+
+  const listBreed = async () => {
+    const response = await fetch("https://dog.ceo/api/breeds/list/all");
+    const data = await response.json();
+    setSrc(data.message);
+    return Object.keys(data.message);
+  };
+
+  useEffect(() => {
+    async function fetchData() {
+      const breeds = await listBreed();
+      setList(
+        breeds.map((breed) => (
+         
+            <button onClick={e =>getImageByBreed(breed)}>{breed}</button>
+       
+        ))
+      );
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Image generater</h1>
+      <div>
+        <button onClick={(e) => getImage()}>Get Image</button>
+      </div>
+      <div>
+        <img src={src} alt="" />
+        {list}
+      </div>
     </div>
   );
 }
